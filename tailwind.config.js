8996b8d6f19737dac0b1649bf0e01883676dcfa9
Michael Boland <michael@boland.onl>
2023-06-08 21:02:15 -0500

    Add fade animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,21 @@ module.exports = {
         'secondary-bg': '#F2F2F2',
         'primary-button': '#DF2266',
         'primary-overlay': '#6610f2'
-      }
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fadeOut: {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out',
+        fadeOut: 'fadeOut 1s ease-in-out',
+      },
     },
   },
   plugins: [
