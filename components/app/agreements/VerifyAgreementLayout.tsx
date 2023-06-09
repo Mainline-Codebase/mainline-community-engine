1065e786e5787a833a7ef4e46137da5bf7edd176
bmeredith <1832575+bmeredith@users.noreply.github.com>
2023-06-09 03:11:59 -0500

    projects can now be verified by chainlink

--- components/app/agreements/VerifyAgreementLayout.tsx
+++ components/app/agreements/VerifyAgreementLayout.tsx
@@ -1,33 +1,72 @@
 'use client';
 
+import { useRef, useState } from 'react';
+import { toast } from "react-toastify"
 import { useAccount } from 'wagmi';
 import SecondaryButton from '../../SecondaryButton';
+import executeRequest from '../../../systems/request/request';
 
 interface Props {
   project: any
 }
 
 function VerifyAgreementLayout({ project }: Props) {
-  const { isConnected } = useAccount();
+  const { isConnected, address } = useAccount();
+  const [isRequesting, setIsRequesting] = useState(false);
+  const notification = useRef(null);
   console.log(project);
 
-  //   const { config } = usePrepareContractWrite({
-  //     address: MCE_CONTRACT_ADDRESS,
-  //     abi: communityEngineABI,
-  //     chainId: 11155111,
-  //     functionName: 'signAgreement',
-  //     account: address,
-  //     // @ts-ignore
-  //     args: [project.owner, project.name],
-  //     enabled: !!address,
-  //   });
+  const updateNotification = (message: any) => {
+    if (notification.current) {
+      // toast.update(notification.current, {
+      //   render: message,
+      //   type: 'info',
+      //   isLoading: true,
+      //   autoClose: false,
+      // });
+    }
+  };
 
-  //   const {
-  //     write,
-  //   } = useContractWrite(config);
+  const performRequest = async () => {
+    // @ts-ignore
+    notification.current = toast.loading('Initiating request...');
+    setIsRequesting(true);
+
+    const res = await executeRequest(
+      [project.owner, project.name, project.kolTwitterHandle, project.tweetKeywords.join()],
+      'sepolia',
+      updateNotification,
+    );
+
+    if (res.error) {
+      console.log('ERROR: ', res.result);
+      const isKnownError = res.result.toString().includes('transaction failed');
+
+      // @ts-ignore
+      // toast.update(notification.current, {
+      //   render: `Request failed. ${isKnownError ? 'This subscription might be out of LINK.' : ''}`,
+      //   type: 'error',
+      //   isLoading: false,
+      //   autoClose: 5000,
+      // });
+    } else {
+      // @ts-ignore
+      // toast.update(notification.current, {
+      //   render: 'Request successful.',
+      //   type: 'success',
+      //   isLoading: false,
+      //   autoClose: 5000,
+      // });
+
+      console.log(res);
+    }
+
+    notification.current = null;
+    setIsRequesting(false);
+  };
 
   return !isConnected ? null : (
-    <SecondaryButton text="Verify" onClick={console.log} />
+    <SecondaryButton text="Verify" onClick={performRequest} /* disabled={isRequesting} */ />
   );
 }
 
